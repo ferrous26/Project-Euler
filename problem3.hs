5f89150403625c26b0a5df3d23c2fefadce2ed05
Mark Rada <marada@uwaterloo.ca>
2009-07-04 15:17:31 -0400

    Decided to optimize problem3 anyways
    Found out from the completion thread that testing for the primeness
    of the divisor is not actually needed, now the program runs in
    essentially zero time.

--- problem3.hs
+++ problem3.hs
@@ -1,18 +1,8 @@
 -- Project Euler/Problem3.hs -- Find the largest prime factor of 600851475143
-
 main :: IO()
 main = putStrLn ( show ( lpf 600851475143 [3,2] ) )
-
+-- Apparently testing for primeness of divisor is not needed
 lpf :: Integer -> [Integer] -> Integer
 lpf x (y:ys) | x == y           = y
              | (x `mod` y) == 0 = lpf (x `div` y) (y:ys)
-             | otherwise        = lpf x (getPrime y (y:ys))
-
-getPrime :: Integer -> [Integer] -> [Integer]
-getPrime x ys | primeTest (x+2) ys = (x+2):ys
-              | otherwise          = getPrime (x+2) ys
-
-primeTest :: Integer -> [Integer] -> Bool
-primeTest x (y:ys) | (x `mod` y) == 0 = False
-                   | y == 3           = True
-                   | otherwise        = primeTest x ys
+             | otherwise        = lpf x ((y+2):(y:ys))
