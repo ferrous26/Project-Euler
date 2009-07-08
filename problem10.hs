0866ac3bbc2c874059481910077c04979e7274b0
Mark Rada <marada@uwaterloo.ca>
2009-07-07 23:28:32 -0400

    Managed to fix the Haskell version of Problem 10
    Sort of, not the same as my C solution, but this cost only one
    more line of code and actually finishes in a few seconds as
    opposed to several minutes.

--- problem10.hs
+++ problem10.hs
@@ -3,5 +3,6 @@ main :: IO()
 main = putStrLn ( show ( sum ( primes [2] ( take 1000000 ( iterate (+2) 3 )))))
 
 primes :: [Integer] -> [Integer] -> [Integer]
-primes xs (y:ys) = primes (y:xs) ( filter ( \z -> (mod z y) /= 0 ) ys )
+primes xs (y:ys) | y > 1414  = primes (y:xs) ys
+                 | otherwise = primes (y:xs) (filter (\z -> (mod z y) /= 0) ys)
 primes xs []     = xs
