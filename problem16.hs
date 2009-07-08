3dd847110d6f88e2a74cd6b2bcf066417cdfe4ac
Mark Rada <marada@uwaterloo.ca>
2009-07-07 23:57:30 -0400

    Optimized, by using a function in the Char module and the map function

--- problem16.hs
+++ problem16.hs
@@ -1,8 +1,4 @@
 -- problem16.hs --- Sum of digits in the number 2^1000
-
+import Char
 main :: IO()
-main = putStrLn ( show ( sumDigits ( 2 ^ 1000 ) 0 ) )
-
-sumDigits :: Integer -> Integer -> Integer
-sumDigits x y | x == 0    = y
-              | otherwise = sumDigits ( div x 10 ) ( y + ( rem x 10 ) )
+main = putStrLn ( show ( sum ( map (digitToInt) ( show ( 2 ^ 1000 ) ) ) ) )
